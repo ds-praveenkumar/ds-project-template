472ea5cb1bfc5083803d88b0d8bc0c8f9cdcee97
ds-praveenkumar <praveenkr2208@gmail.com>
2020-11-05 00:46:56 +0530

    update for lint

--- config/config.py
+++ config/config.py
@@ -15,20 +15,17 @@ class Config:
     """ stores default configuration """
 
 
-    ROOT_DIR=Path(Path.cwd()).resolve().as_posix()
-    DATA_DIR=Path(ROOT_DIR) / "data"
-    RAW_DATA=Path(DATA_DIR) / "raw"
-    PROCESSED_DATA=Path(DATA_DIR) / "processed"
-    CLAENED_DATA=Path(DATA_DIR) / "cleaned"
+    ROOT_DIR = Path(Path.cwd()).resolve().as_posix()
+    DATA_DIR = Path(ROOT_DIR) / "data"
+    RAW_DATA = Path(DATA_DIR) / "raw"
+    PROCESSED_DATA = Path(DATA_DIR) / "processed"
+    CLAENED_DATA = Path(DATA_DIR) / "cleaned"
 
-    MODELS_DIR=Path(ROOT_DIR) / "models"
-    NB_DIR=Path(ROOT_DIR) / "notebooks"
-    DOCS_DIR=Path(ROOT_DIR) / "docs"
-                
-    SRC_DIR=Path(ROOT_DIR) / "src"
-    BUILD_DIR=Path(SRC_DIR.as_posix()) / "build"
-    PREPARE_DIR=Path(SRC_DIR.as_posix()) / "prepare"
-    PREDICT_DIR=Path(SRC_DIR.as_posix()) / "predict"   
-
-
-
+    MODELS_DIR = Path(ROOT_DIR) / "models"
+    NB_DIR = Path(ROOT_DIR) / "notebooks"
+    DOCS_DIR = Path(ROOT_DIR) / "docs" 
+               
+    SRC_DIR = Path(ROOT_DIR) / "src" 
+    BUILD_DIR = Path(SRC_DIR.as_posix()) / "build" 
+    PREPARE_DIR = Path(SRC_DIR.as_posix()) / "prepare" 
+    PREDICT_DIR = Path(SRC_DIR.as_posix()) / "predict"   
